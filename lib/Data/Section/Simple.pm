ec49232a0c7ee3d057907b6d539ffe47acab9c60
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-09-18 19:37:36 +0000

    Import of MIYAGAWA/Data-Section-Simple-0.03 from CPAN.
    gitpan-cpan-distribution: Data-Section-Simple
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MIYAGAWA/Data-Section-Simple-0.03.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- lib/Data/Section/Simple.pm
+++ lib/Data/Section/Simple.pm
@@ -2,7 +2,7 @@ package Data::Section::Simple;
 
 use strict;
 use 5.008_001;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use base qw(Exporter);
 our @EXPORT_OK = qw(get_data_section);
@@ -76,7 +76,7 @@ Data::Section::Simple - Read data from __DATA__
 
 =head1 DESCRIPTION
 
-Data::Section::Simple is a simple module to extract data drom
+Data::Section::Simple is a simple module to extract data from
 C<__DATA__> section of the file.
 
 =head1 LIMITATIONS
@@ -94,18 +94,29 @@ caller code.
 
 =head1 BUGS
 
+=head2 __DATA__ appearing elsewhere
+
 If you data section has literal C<__DATA__> in the data section, this
 module might be tricked by that. Although since its pattern match is
 greedy, C<__DATA__> appearing I<before> the actual data section
 (i.e. in the code) might be okay.
 
-This is by design -- in thoery you can C<tell> the DATA handle before
+This is by design -- in theory you can C<tell> the DATA handle before
 reading it, but then reloading the data section of the file (handy for
 developing inline templates with PSGI web applications) would fail
 because the pos would be changed.
 
 If you don't like this design, again, use the superior
 L<Data::Section>.
+
+=head2 utf8 pragma
+
+If you enable L<utf8> pragma in the caller's package (or the package
+you're inspecting with the OO interface), the data retrieved via
+C<get_data_section> is decoded, but otherwise undecoded. There's no
+reliable way for this module to programmatically know whether utf8
+pragma is enabled or not: it's your responsibility to handle them
+correctly.
 
 =head1 AUTHOR
 
